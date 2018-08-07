18fa593c9e3024ec733172f0d3d4636a55ed4308
Asus <Asus@CP-1004>
2018-08-07 13:35:27 +0700

    GUI Setting + Chat

---
+++ src/chataja/main/Chataja.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chataja.main;
+
+import chataja.view.frm_login;
+
+/**
+ *
+ * @author Asus
+ */
+public class Chataja {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frm_login login = new frm_login();
+        login.setVisible(true);
+    }
+    
+}
